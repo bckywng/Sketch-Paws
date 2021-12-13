96668d4f6328158b912437b647100b2368d768b2
bckywng <beckywongis@gmail.com>
2021-12-13 00:56:55 -0500

    Detail View Styled

--- Sketch+Paws/DetailView.swift
+++ Sketch+Paws/DetailView.swift
@@ -8,12 +8,40 @@
 import SwiftUI
 
 struct DetailView: View {
+    let pose: Pose
     var body: some View {
         ZStack {
             Color("Secondary")
                 .ignoresSafeArea()
-            Text("Purrfect")
-                .font(.title)
+            ScrollView (showsIndicators: false){
+                VStack(spacing:20) {
+                    Image(pose.icon)
+                        .resizable()
+                        .frame(width: 300, height: 300)
+                    Text(pose.name)
+                        .font(.system(size: 36))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("Highlight"))
+                    Text(pose.asana)
+                        .italic()
+                        .fontWeight(.medium)
+                    Text(pose.description)
+                    Text("How to:")
+                        .foregroundColor(Color("Highlight"))
+                        .fontWeight(.medium)
+                    ForEach(pose.steps, id:\.self){
+                        step in
+                        Text(step)
+                    }
+                    Text("Top tip:")
+                        .foregroundColor(Color("Highlight"))
+                        .fontWeight(.medium)
+                    Text(pose.topTip)
+                }
+                .padding(.horizontal, 20)
+                .multilineTextAlignment(.center)
+            }
+
         }
     }
 }
@@ -21,6 +49,14 @@ struct DetailView: View {
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(pose: Pose(
+            name: "Downward-Facing Dog",
+            asana: "Adho Mukha Shvanasana",
+            icon: "cat_01",
+            description: "Did someone say dog? Can't we call this a downward-facing cat instead? It's OK – this is a friendly dog, it's not interested in chasing cats. In fact, Downward-Facing Dog is the lynchpin of a yoga asana practice: if you're going to befriend any of these poses, make sure it's this canine classic.",
+            steps: ["From a kneeling position, place your hands shoulder-distance apart and spread your fingers.","Tuck your toes and lift your hips up towards the ceiling so you create an inverted V shape.", "Balance the weight between hands and feet and think about tilting your tailbone up towards the ceiling.", "Send your gaze towards your feet and breathe!"],
+            topTip: "bend your knees in order to create more length through the spine."
+        )
+        )
     }
 }
